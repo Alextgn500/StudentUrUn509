367c469d10314ca5b395e80cf59fb88774518ef6
Александр <azubkov500@gmail.com>
2024-12-26 12:46:31 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -1,5 +1,6 @@
 import sqlite3
 from texts14 import PRODUCTS
+from texts14 import USERS
 
 
 def initiate_db():
@@ -8,7 +9,7 @@ def initiate_db():
 
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Products(
-        id INT PRIMARY KEY,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
         description TEXT,
         price INT NOT NULL,
@@ -16,11 +17,30 @@ def initiate_db():
     );
     ''')
 
-    cursor.executemany('INSERT INTO Products VALUES (?, ?, ?, ?, ?)', PRODUCTS)
-    connection.commit()
+    cursor.executemany(
+        'INSERT INTO Products (title, description, price, image_path) VALUES (?, ?, ?, ?)',
+        [(title, desc, price, img) for _, title, desc, price, img in PRODUCTS]
+    )
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INT NOT NULL,
+        balance INT DEFAULT 1000
+    );
+    ''')
+
+    cursor.executemany(
+        'INSERT INTO Users (username, email, age) VALUES (?, ?, ?)',
+        [(username, email, age) for _, username, email, age in USERS]
+    )
 
     connection.commit()
     connection.close()
+
+
 
 def get_all_products():
     connection = sqlite3.connect('products.db')
@@ -33,6 +53,49 @@ def get_all_products():
     return products
 
 
+def add_user(username: str, email: str, age: int):
+    try:
+        with sqlite3.connect('products.db') as connection:
+            cursor = connection.cursor()
+
+            # Добавляем пользователя
+            cursor.execute(
+                'INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, 1000)',
+                (username, email, age)
+            )
+
+            # Добавляем commit внутри try блока
+            connection.commit()
+
+            # Для проверки  добавлен вывод последнего ID
+            last_id = cursor.lastrowid
+            print(f"Пользователь успешно добавлен с ID: {last_id}")
+
+    except sqlite3.Error as e:
+        print(f"Ошибка при добавлении пользователя: {e}")
+
+
+# Для проверки добавлена функция
+def get_all_users():
+    with sqlite3.connect('products.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM Users')
+        return cursor.fetchall()
+
+
+def is_included(username):
+    connection = sqlite3.connect('products.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT COUNT(*) FROM Users WHERE username = ?', (username,))
+    result = cursor.fetchone()[0] > 0
+
+    connection.close()
+    return result
+
+
 if __name__ == "__main__":
     initiate_db()
     print("База данных создана!")
+
+
